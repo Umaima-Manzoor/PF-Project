d995f7ef6e76ddc74327a9c5d10f0da64ea0d0b8
aimanxmisbah <aimanmisbah06@gmail.com>
2024-11-11 10:13:38 +0500

    Update clearcontacts.cpp

--- clearcontacts.cpp
+++ clearcontacts.cpp
@@ -1,14 +1,11 @@
 #include<stdio.h>
 
 void ClearContact(name[][],email[][],number[][]){
-	int n1,n2;
-	printf("Enter from which contact you want you delete till the last contact: ");
-	scanf("%d %d",&n1,&n2);
 	
-	for(int i=n1 ;i<n2 ;i++){
-		strcpy(name[i], "");
-        strcpy(email[i], "");
-        strcpy(number[i], "");
+	for(int i=0 ;i<1000 ;i++){
+		strcpy(name[i], " ");
+        strcpy(email[i], " ");
+        strcpy(number[i], " ");
 	}
 	
 }
