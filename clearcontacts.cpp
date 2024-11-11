9fb1ae3ad941dc9e409d13866fc457d3768f3b86
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2024-11-11 10:44:55 +0500

    Update clearcontacts.cpp

--- clearcontacts.cpp
+++ clearcontacts.cpp
@@ -1,11 +1,12 @@
 #include<stdio.h>
 
-void ClearContact(name[][],email[][],number[][]){
+
+void ClearContact(char (*name)[1000][100],char (*email)[1000][100], char (*number)[1000][100]){
 	
 	for(int i=0 ;i<1000 ;i++){
-		strcpy(name[i], " ");
-        strcpy(email[i], " ");
-        strcpy(number[i], " ");
+		strcpy((*name)[i], " ");
+        strcpy((*email)[i], " ");
+        strcpy((*number)[i], " ");
 	}
 	
 }
