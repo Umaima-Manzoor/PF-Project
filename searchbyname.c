8d5dbc680e68d1e8fd97ce2e23e072679086674b
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2024-11-11 10:50:15 +0500

    Update searchbynumber.c

--- searchbyname.c
+++ searchbyname.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
-void SearchByNumber(char name[][],char email[][],char number[][],char num[]){
+void SearchByNumber(char name[1000][100],char email[1000][100],char number[1000][100]){
 
+    char num[100];
 	printf("Enter the phone number you would like to search your contact by: ");
 	scanf("%s", num);
 
-	int flag = 0;
+	int flag = 0, index;
 	
 	for(int i=0 ;i<1000;i++){
 	
-	 if(strcmp(num, number[i])) {
-	 	flag = 1;	
-	    index=i;  
-	 }
+        if(strcmp(num, number[i])==0) {
+            flag = 1;	
+            index=i; 
+            break; 
+        }
 	 }
 	 
 	 if (flag){
-	 	printf(" %s %s %s",name[index],email[index],number[index]);
+	 	printf("Name: %s\nEmail: %s\nPhone Number: %s\n",name[index],email[index],number[index]);
 	 	
 	 }
 	 else 
 	 printf("Contact is not in the list");
 }
+
+
 
 
 
