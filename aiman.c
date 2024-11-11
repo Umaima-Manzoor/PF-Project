037e8f0ac1143ed4fade88cd45673762d822aaaf
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2024-11-11 11:04:02 +0500

    Update aiman.c

--- aiman.c
+++ aiman.c
@@ -1,21 +1,29 @@
-int count=0;
-
-void AddContact(char name[1000][100], char email[1000][100], char number[1000][100])
+void AddContact(char (*name)[1000][100], char (*email)[1000][100], char (*number)[1000][100])
 {
-    printf("Enter Name: ");
-    fgets(name[count], 100, stdin);
-    name[count][strcspn(name[count], "\n")] = '\0';
+    int i, flag=0;;
+    for (i=0; i<1000; i++) {
+        if (strcmp((*name)[i], " ") == 0) {
+            flag=1;
+            printf("Enter Name: ");
+            fgets((*name)[i], 100, stdin);
+            (*name)[i][strcspn((*name)[i], "\n")] = '\0';
 
-    printf("Enter Email: ");
-    fgets(email[count], 100, stdin);
-    email[count][strcspn(email[count], "\n")] = '\0';
+            printf("Enter Email: ");
+            fgets((*email)[i], 100, stdin);
+            (*email)[i][strcspn((*email)[i], "\n")] = '\0';
 
-    printf("Enter Phone Number: ");
-    fgets(number[count], 100, stdin);
-    number[count][strcspn(number[count], "\n")] = '\0';
+            printf("Enter Phone Number: ");
+            fgets(number[i], 100, stdin);
+            (*number)[i][strcspn((*number)[i], "\n")] = '\0';
 
-    count++;
-    puts("Contact Was Added Successfully!");
+            puts("Contact Was Added Successfully!");
+            break;
+        }
+    }
+
+    if (flag==0) {
+        puts("Sorry, the contact list is full");
+    }
 }
 
 void SearchByName(char name[1000][100], char email[1000][100], char number[1000][100])
@@ -27,19 +35,19 @@ void SearchByName(char name[1000][100], char email[1000][100], char number[1000]
     fgets(SearchName, 100, stdin);
     SearchName[strcspn(SearchName, "\n")] = '\0';
     int i;
-    for(i=0;i<count;i++) 
+    for(i=0;i<1000;i++) 
 	{
         if(strcmp(SearchName,name[i])==0) 
 		{
-            puts("Contact Found:");
             printf("Name: ");
             puts(name[i]);
             printf("Email: ");
             puts(email[i]);
             printf("Phone Number: ");
             puts(number[i]);
+            printf("\n");
             flag = 1;
-            break;
+
         }
     }
 
@@ -49,24 +57,25 @@ void SearchByName(char name[1000][100], char email[1000][100], char number[1000]
     }
 }
 
+
 void DisplayContacts(char name[1000][100], char email[1000][100], char number[1000][100]) 
 {
-    if(count==0)
+    int i, flag=0;;
+    for(i=0;i<1000;i++) 
 	{
-        puts("No Contacts Present");
-        return;
+        if (strcmp(name[i], " ")!=0) {
+            flag=1;
+            printf("Contact %d:\n", i + 1);
+            printf("Name: ");
+            puts(name[i]);
+            printf("Email: ");
+            puts(email[i]);
+            printf("Phone Number: ");
+            puts(number[i]);
+            printf("\n");
+        }
     }
-    int i;
-    for(i=0;i<count;i++) 
-	{
-        printf("Contact %d:\n", i + 1);
-        printf("Name: ");
-        puts(name[i]);
-        printf("Email: ");
-        puts(email[i]);
-        printf("Phone Number: ");
-        puts(number[i]);
-        printf("\n");
+    if (flag==0) {
+        printf("No contacts found");
     }
 }
-
