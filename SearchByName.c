926f70d6cd1ed2b83b2a95d311e79e8c432a79e9
Aiman Misbah <aimanmisbah06@gmail.com>
2024-11-16 13:59:01 +0500

    Update SearchByName.c

--- SearchByName.c
+++ SearchByName.c
@@ -1,31 +1,29 @@
 void SearchByName(char name[1000][100], char email[1000][100], char number[1000][100])
 {
     char SearchName[100];
-    int flag=0;
+    int flag = 0;
 
-    printf("Enter the name of the contact you want to search: ");
-    fgets(SearchName, 100, stdin);
-    SearchName[strcspn(SearchName, "\n")] = '\0';
+    printf("\nEnter the name of the contact you want to search: ");
+    scanf(" %[^\n]", SearchName);
+
     int i;
-    for(i=0;i<count;i++) 
-	{
-        if(strcmp(SearchName,name[i])==0) 
-		{
-            puts("Contact Found:");
-            printf("Name: ");
+    for (i = 0; i < 1000; i++)
+    {
+        if (strcmp(SearchName, name[i]) == 0)
+        {
+            printf("\nName: ");
             puts(name[i]);
             printf("Email: ");
             puts(email[i]);
             printf("Phone Number: ");
             puts(number[i]);
+            printf("\n");
             flag = 1;
-            break;
         }
     }
 
-    if (!flag) 
-	{
-        puts("Contact Not Found");
+    if (!flag)
+    {
+        printf("\nSorry, but no contact was found in the database with the name '%s'\n", SearchName);
     }
 }
-
