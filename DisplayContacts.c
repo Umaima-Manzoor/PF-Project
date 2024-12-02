619b07ac34fc4be74557dfb4d7204591e56fa278
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2024-12-03 00:11:20 +0500

    Update DisplayContacts.c

--- DisplayContacts.c
+++ DisplayContacts.c
@@ -1,21 +1,28 @@
-void DisplayContacts(char name[1000][100], char email[1000][100], char number[1000][100]) 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void DisplayContacts(char name[1000][100], char email[1000][100], char number[1000][100])
 {
-    if(count==0)
-	{
-        puts("No Contacts Present");
-        return;
+    int i, flag = 0;
+
+    for (i = 0; i < 1000; i++)
+    {
+        if (strcmp(name[i], " ") != 0)
+        {
+            flag = 1;
+            printf("Contact %d:\n", i + 1);
+            printf("Name: ");
+            puts(name[i]);
+            printf("Email: ");
+            puts(email[i]);
+            printf("Phone Number: ");
+            puts(number[i]);
+            printf("\n");
+        }
     }
-    int i;
-    for (i=0;i<count; i++) 
-	{
-        printf("Contact %d:\n", i + 1);
-        printf("Name: ");
-        puts(name[i]);
-        printf("Email: ");
-        puts(email[i]);
-        printf("Phone Number: ");
-        puts(number[i]);
-        printf("\n");
+    if (flag == 0)
+    {
+        printf("No contacts found");
     }
 }
-
