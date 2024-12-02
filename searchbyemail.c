c4bdd584a75d90dbfc27d08d6757dd355596391f
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2024-12-03 00:10:39 +0500

    Update searchbyemail.c

--- searchbyemail.c
+++ searchbyemail.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-void SearchByEmail(char name[1000][100], char email[1000][100], char num[1000][100]) {
-    int i,flag=0;
+int SearchByEmail(char name[1000][100], char email[1000][100], char number[1000][100], int *position)
+{
     char search[100];
-    printf("Enter the email you would like to search the contacts by: ");
-    scanf(" %s", search);
+    int i, flag = 0;
+    int count = 0;
+    char temp;
 
-    for (i=0; i<1000; i++) {
-        if (strcmp(email[i], " ") != 0) {
-            if (strcmp(email[i], search) == 0) {
-                printf("Name: %s\nEmail Address: %s\nPhone Number: %s\n\n", name[i], email[i], num[i]);
-                flag=1;
-            }
+    printf("\nEnter the email you would like to search the contacts by: ");
+    scanf(" %[^\n]", search);
+    while((temp = getchar()) != '\n' && temp != EOF);
 
+    for (i = 0; i < 1000; i++)
+    {
+        if (strstr(email[i], search) != NULL && strcmp(email[i], " ") != 0)
+        {
+            position[count] = i;
+            printf("\nContact %d:\n", count + 1);
+            count++;
+            printf("Name: %s\nEmail Address: %s\nPhone Number: %s\n\n", name[i], email[i], number[i]);
+            flag = 1;
         }
     }
 
-    if (flag==0) {
-        printf("Sorry, but no contact was found in the database with the email address '%s'\n", search);
+    if (!flag)
+    {
+        printf("\nSorry, but no contact was found in the database with the email address '%s'\n", search);
     }
-    
 
+    return count;
 }
