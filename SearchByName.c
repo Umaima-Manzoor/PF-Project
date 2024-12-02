f557477d7b1d9a5f8c88420c38c1ce5042f4741d
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2024-12-03 00:09:20 +0500

    Update SearchByName.c

--- SearchByName.c
+++ SearchByName.c
@@ -1,29 +1,54 @@
-void SearchByName(char name[1000][100], char email[1000][100], char number[1000][100])
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int SearchByName(char name[1000][100], char email[1000][100], char number[1000][100], int *position)
 {
     char SearchName[100];
-    int flag = 0;
+    int flag = 0, i;
+    char temp;
+    int validName;
+    int count = 0;
 
-    printf("\nEnter the name of the contact you want to search: ");
-    scanf(" %[^\n]", SearchName);
+    do
+    {
+        printf("\nEnter the name of the contact you want to search: ");
+        scanf(" %[^\n]", SearchName);
+        while((temp = getchar()) != '\n' && temp != EOF);
 
-    int i;
+        validName = 1;
+        for (i = 0; SearchName[i] != '\0'; i++)
+        {
+            if (!((SearchName[i] >= 'a' && SearchName[i] <= 'z') || 
+                  (SearchName[i] >= 'A' && SearchName[i] <= 'Z') || 
+                  SearchName[i] == ' '))
+            {
+                validName = 0;
+                break;
+            }
+        }
+
+        if (!validName)   
+        {
+            printf("Error: Invalid name! Only alphabets (both upper and lower cases) are allowed.\n\n");
+        }
+
+    } while (!validName);
+
     for (i = 0; i < 1000; i++)
     {
-        if (strcmp(SearchName, name[i]) == 0)
+        if (strstr(name[i], SearchName) != NULL && strcmp(name[i], " ") != 0)
         {
-            printf("\nName: ");
-            puts(name[i]);
-            printf("Email: ");
-            puts(email[i]);
-            printf("Phone Number: ");
-            puts(number[i]);
-            printf("\n");
+            position[count] = i;
+            printf("\nContact %d:\n", count + 1);
+            count++;
+            printf("Name: %s\nEmail: %s\nPhone Number: %s\n\n", name[i], email[i], number[i]);
             flag = 1;
         }
     }
-
     if (!flag)
     {
         printf("\nSorry, but no contact was found in the database with the name '%s'\n", SearchName);
     }
+    return count;
 }
