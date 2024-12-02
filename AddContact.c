059caa1242f43b7043221b6be8dfb63a172bee84
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2024-12-03 00:07:18 +0500

    Update AddContact.c

--- AddContact.c
+++ AddContact.c
@@ -1,16 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
 void AddContact(char (*name)[1000][100], char (*email)[1000][100], char (*number)[1000][100])
 {
     int i, flag = 0;
     char n[100], e[100], num[100];
+    int validName;
 
-    printf("Enter Name: ");
-    scanf(" %[^\n]", n);
+    do
+    {
+        printf("Enter Name: ");
+        scanf(" %[^\n]", n);
+
+        validName = 1;
+
+        for (i = 0; n[i] != '\0'; i++)
+        {
+            if (!((n[i] >= 'a' && n[i] <= 'z') || (n[i] >'A' && n[i] <= 'Z')|| n[i]==' '))
+            {
+                validName = 0;
+                break;
+            }
+        }
+
+        if (!validName)   
+        {
+            printf("Error: Invalid name! Only alphabets (both upper and lower cases) are allowed.\n\n");
+        }
+
+    } while (!validName);
 
     printf("Enter Email: ");
     scanf(" %[^\n]", e);
 
-    printf("Enter Phone Number: ");
-    scanf(" %[^\n]", num);
+    int validPhone;
+
+    do
+    {
+        printf("Enter Phone Number: ");
+        scanf(" %[^\n]", num);
+
+        validPhone = 1;
+
+        for (i = 0; num[i] != '\0'; i++)
+        {
+            if (!((num[i] >= '0' && num[i] <= '9') || num[i] == '+' || num[i] == '-'))
+            {
+                validPhone = 0;
+                break;
+            }
+        }
+
+        if (!validPhone)   
+        {
+            printf("Error: Invalid phone number! Only digits, '+' and '-' are allowed.\n\n");
+        }
+
+    } while (!validPhone);
 
     for (i = 0; i < 1000; i++)
     {
